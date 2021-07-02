1ec240b1c87f53164ed4dab64d25948a8f7edd1a
adityadangre <75615989+adityadangre@users.noreply.github.com>
2021-07-02 18:01:00 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Feed extends React.Component {
+    render() {
+        return (
+            <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 250}}>
+                <Text>Feed Screen</Text>
+            </View>
+        )
+    }
+}
